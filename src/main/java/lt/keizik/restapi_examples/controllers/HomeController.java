429ef88e4957d3139071444374e806786bd79371
Aleksandr Keizik <alex.keizik@gmail.com>
2024-12-05 12:54:35 +0200

    Add openapi annotatons

--- src/main/java/lt/keizik/restapi_examples/controllers/HomeController.java
+++ src/main/java/lt/keizik/restapi_examples/controllers/HomeController.java
@@ -9,25 +9,32 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lt.keizik.restapi_examples.dto.Message;
 import lt.keizik.restapi_examples.dto.MessageForm;
 import lt.keizik.restapi_examples.dto.MessageUpdateForm;
 
 @RestController
+@Tag(name = "Home Page")
 public class HomeController {
     @GetMapping
+    @Operation(description = "Home page info", summary = "Home page info")
     public String getHomePage() {
         return "This is home page";
     }
 
     // POST
     @PostMapping
+    @Operation(description = "Create a message", summary = "Create a message")
     public Message create(@RequestBody MessageForm form) {
         return new Message(1L, form.getMessage());
     }
     
     // PUT
     @PutMapping("/{id}")
+    @Operation(description = "Update a message", summary = "Update a message")
     public Message update(@PathVariable String id, @RequestBody MessageUpdateForm form) {
         var message = new Message(Long.valueOf(id), "Old message");
         message.setResponse(form.getMessages().get(1));
@@ -36,7 +43,8 @@ public class HomeController {
     
     // DELETE
     @DeleteMapping("/{id}")
-    public Message delete(@PathVariable Long id) {
+    @Operation(description = "Delete a message", summary = "Delete a message")
+    public Message delete(@PathVariable @Parameter(description = "Id of a message", example = "1") Long id) {
         return new Message(id, "Deleted");
     }
 }
--- src/main/java/lt/keizik/restapi_examples/dto/MessageForm.java
+++ src/main/java/lt/keizik/restapi_examples/dto/MessageForm.java
@@ -1,8 +1,10 @@
 package lt.keizik.restapi_examples.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class MessageForm {
+    @Schema(description = "New message", example = "My message")
     private String message;
 }
