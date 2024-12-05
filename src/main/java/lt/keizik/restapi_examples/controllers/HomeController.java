cadc2951ed6aca615e36183c01ebb064495b42c6
Aleksandr Keizik <alex.keizik@gmail.com>
2024-12-05 12:03:39 +0200

    Add Post

--- src/main/java/lt/keizik/restapi_examples/controllers/HomeController.java
+++ src/main/java/lt/keizik/restapi_examples/controllers/HomeController.java
@@ -4,7 +4,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import lt.keizik.restapi_examples.dto.MessageForm;
 
 @RestController
 public class HomeController {
@@ -15,8 +18,8 @@ public class HomeController {
 
     // POST
     @PostMapping
-    public String create() {
-        return "Something was created";
+    public String create(@RequestBody MessageForm form) {
+        return form.getMessage();
     }
     
     // PUT
---
+++ src/main/java/lt/keizik/restapi_examples/dto/MessageForm.java
@@ -0,0 +1,8 @@
+package lt.keizik.restapi_examples.dto;
+
+import lombok.Data;
+
+@Data
+public class MessageForm {
+    private String message;
+}
